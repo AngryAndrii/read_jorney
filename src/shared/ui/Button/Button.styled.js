6211da6628ea07336330363e3c553e62caff0fee
Andrii Chychkan <mywork1728@gmail.com>
2024-05-13 17:05:02 +0300

    added svgr, buttons, icons

--- src/shared/ui/Button/Button.styled.js
+++ src/shared/ui/Button/Button.styled.js
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
-  background-color: black;
+export const StyledButton = styled.button`
+  width: 100%;
+  height: 100%;
+`;
+
+export const StyledRegisterButton = styled(StyledButton)`
+  background-color: var(--color-secondary);
+  color: var(--color-primary);
+  padding: 12px 29px;
+  @media screen and (min-width: 768px) {
+    padding: 16px 54px;
+  }
+`;
+
+export const StyledLoginButton = styled(StyledRegisterButton)`
+  padding: 12px 45px;
+`;
+
+export const StyledBlackButton = styled(StyledButton)`
+  background-color: var(--color-primary);
+  color: var(--color-secondary);
+  padding: 10px 20px;
+  @media screen and (min-width: 768px) {
+    padding: 14px 28px;
+  }
 `;
