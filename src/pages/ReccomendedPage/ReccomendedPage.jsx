ecda9814af2cd41273eecc31bef325fd1081d31e
Andrii Chychkan <mywork1728@gmail.com>
2024-07-24 16:57:29 +0300

    added navbar

--- src/pages/ReccomendedPage/ReccomendedPage.jsx
+++ src/pages/ReccomendedPage/ReccomendedPage.jsx
@@ -1,9 +1,11 @@
-import { post } from 'src/app/api/api';
+import { Wrapper } from 'src/shared';
+import { Navbar } from 'src/widgets/navbar/Navbar';
 
 export const ReccomendedPage = () => {
   return (
-    <>
+    <Wrapper>
+      <Navbar></Navbar>
       <>Reccomended page</>
-    </>
+    </Wrapper>
   );
 };
